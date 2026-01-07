f8b5787e4b13993b7fe9fa8b5e10dcae81b636d5
JhonatanAbrigo11 <jhonatanabrigo2018@gmail.com>
2026-01-06 23:39:55 -0500

    Primer controller en inventario

---
+++ inventario/src/main/java/com/krakedev/inventario/saludoController.java
@@ -0,0 +1,23 @@
+package com.krakedev.inventario;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+/* 
+    Controlador REST: Es una clase en Java que responde rutas HTTP (Urls) para que otros sistemas puedan 
+    enviarle peticiones y recibir respuestas em formato JSON.
+*/
+@RestController // Indica que esta clase es un controlador REST
+
+@RequestMapping("/micontroller") //Define la ruta base para todas las rutas en este controlador
+
+public class saludoController {
+
+    @GetMapping("/saludo") //Mapea las peticiones GET a /micontroller/saludo
+    public String saludar() {
+        return "Hola desde Spring Boot";
+    }   
+}
