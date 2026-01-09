9918e901db8741a57dba66c4bf88456be662db61
JhonatanAbrigo11 <jhonatanabrigo2018@gmail.com>
2026-01-08 23:24:42 -0500

    Método crear implementado

--- inventario/src/main/java/com/krakedev/inventario/controller/MessageController.java
+++ inventario/src/main/java/com/krakedev/inventario/controller/MessageController.java
@@ -3,8 +3,13 @@ package com.krakedev.inventario.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,20 @@ public class MessageController {
     @GetMapping
     public List<Message> listarMensajes() {
         return mensajes;
+    }
+
+    @GetMapping("/{id}")
+    public Message obtenerMensajePorId(@PathVariable int id) {
+        Optional <Message> mensaje = mensajes.stream()
+                .filter(m -> m.getId() == id)
+                .findFirst();
+        return mensaje.orElse(null);
+    }
+
+    @PostMapping
+    public Message crearMensaje(@RequestBody Message mensaje) {
+        mensajes.add(mensaje);
+        return mensaje;
+
     }
 }
