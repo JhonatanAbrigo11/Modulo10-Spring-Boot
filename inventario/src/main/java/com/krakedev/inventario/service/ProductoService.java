5ea366a70afc67cc1a8498a06a03976761a25c64
JhonatanAbrigo11 <jhonatanabrigo2018@gmail.com>
2026-01-10 15:58:36 -0500

    Métodos y Servicios del Producto

---
+++ inventario/src/main/java/com/krakedev/inventario/service/ProductoService.java
@@ -0,0 +1,22 @@
+package com.krakedev.inventario.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.krakedev.inventario.entity.EstadoProducto;
+import com.krakedev.inventario.entity.Producto;
+
+public interface ProductoService {
+
+    Producto registrarProducto(Producto producto);
+    List<Producto> listarProductos();
+    Optional<Producto> buscarPorIdNombre(String nombre);
+    Optional<Producto> buscarPorId(Long idProducto);
+    Producto actualizarProducto(Long idProducto, Producto producto);
+    void eliminarProducto(Long idProducto);
+
+    Producto cambiarEstadoProducto(Long idProducto, EstadoProducto nuevoEstado);
+    List<Producto> obtenerPorEstadoProducto(EstadoProducto estadoProducto);
+    
+    
+}   
