2afaf44372633c3d3ebd91770848ba0052955dea
JhonatanAbrigo11 <jhonatanabrigo2018@gmail.com>
2026-01-11 12:36:59 -0500

    Método Actualizar y Eliminar

--- inventario/src/main/java/com/krakedev/inventario/controller/ProductoController.java
+++ inventario/src/main/java/com/krakedev/inventario/controller/ProductoController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,4 +54,30 @@ public class ProductoController {
 
     }
 
+    @PutMapping("/actualizar/{idProducto}")
+    public ResponseEntity<?> actualizarProducto(@PathVariable Long idProducto, @RequestBody Producto producto){
+        try{
+            Producto productoActualizado = new Producto();
+            productoActualizado.setNombreProducto(producto.getNombreProducto());
+            productoActualizado.setDescripcion(producto.getDescripcion());
+            productoActualizado.setPrecio(producto.getPrecio());
+            productoActualizado.setCantidad(producto.getCantidad());
+            productoActualizado.setEstadoProducto(producto.getEstadoProducto());
+
+            Producto productoBDD = productoService.actualizarProducto(idProducto, productoActualizado);
+            return ResponseEntity.ok(productoBDD);
+        }catch(Exception exception){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        }
+    }
+
+    @DeleteMapping("/eliminar/{idProducto}")
+    public ResponseEntity<?> eliminarProducto(@PathVariable Long idProducto){
+        try{
+            productoService.eliminarProducto(idProducto);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }catch ( Exception exception){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        }
+    }
 }
